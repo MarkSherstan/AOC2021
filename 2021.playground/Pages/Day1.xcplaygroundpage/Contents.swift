0a8a4e54e5ff31209da947a7f5c917d301a9c9ce
Mark Sherstan <mshersta@ualberta.ca>
2021-12-14 22:24:46 -0700

    Day 1 Part 2

--- 2021.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -5,14 +5,26 @@ let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
 let text = try String(contentsOf: url).split(separator: "\n")
 let sonarDepths = text.compactMap{ Int($0) }
 
-// Count depth increases
-var depthCount = 0
+// Window the depths
+var sonarDepthsWindow = [Int]()
 
-for i in 0..<(sonarDepths.count-1) {
-    if sonarDepths[i+1] - sonarDepths[i] > 0 {
-        depthCount += 1
+for i in 0..<(sonarDepths.count-2) {
+    sonarDepthsWindow.append(sonarDepths[i] + sonarDepths[i+1] + sonarDepths[i+2])
+}
+
+// Depth counter function
+func depthCounter(sonarDepthArray: Array<Int>) -> Int {
+    var depthCount = 0
+    
+    for i in 0..<(sonarDepthArray.count-1) {
+        if sonarDepthArray[i+1] - sonarDepthArray[i] > 0 {
+            depthCount += 1
+        }
     }
+    
+    return depthCount
 }
 
 // Print result
-print(depthCount)
+print("Part 1: ", depthCounter(sonarDepthArray: sonarDepths))
+print("Part 2: ", depthCounter(sonarDepthArray: sonarDepthsWindow))
