5759afe5c644de1fe2f9fdc00bae5fc6689c5a1f
Mark Sherstan <mshersta@ualberta.ca>
2021-12-19 15:59:07 -0700

    Day 6 Part 1 - Slow

---
+++ 2021.playground/Pages/Day6.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+// Import data
+let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
+let timers = try String(contentsOf: url).replacingOccurrences(of: "\n", with: "").split(separator: ",").compactMap{ Int($0) }
+
+// Initial conditions
+class LanternFish {
+    var timer: Int
+    
+    init(timer: Int) {
+        self.timer = timer
+    }
+        
+    func age() -> Bool {
+        if self.timer == 0 {
+            self.timer = 6
+            return true
+        } else {
+            self.timer -= 1
+            return false
+        }
+    }
+}
+
+var lanternFish = [LanternFish]()
+for timer in timers {
+    lanternFish.append(LanternFish(timer: timer))
+}
+
+// Loop through 80 days
+for _ in 0..<80 {
+    for fish in lanternFish {
+        if fish.age() {
+            lanternFish.append(LanternFish(timer: 8))
+        }
+    }
+}
+
+print("Part 1: ", lanternFish.count)
