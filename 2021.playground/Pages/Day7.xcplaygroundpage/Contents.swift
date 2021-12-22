ce8e5f41bb374e94b394d5c7a62fec5b393bb485
Mark Sherstan <mshersta@ualberta.ca>
2021-12-21 21:54:04 -0700

    Day 7 Part 2 (Speed)

--- 2021.playground/Pages/Day7.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day7.xcplaygroundpage/Contents.swift
@@ -1,29 +1,10 @@
 import Foundation
 
-// Import data
-let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
-let crabPosition = try String(contentsOf: url).replacingOccurrences(of: "\n", with: "").split(separator: ",").compactMap{ Int($0) }
+let startTime = NSDate()
 
-// Precalculate sums
-var sumDict: [Int: Int] = [:]
-var sum = 0
+PlayGround.run()
 
-for i in 0...crabPosition.max()! {
-    sum += i
-    sumDict[i] = sum
-}
+let endTime = NSDate()
+let interval = endTime.timeIntervalSince(startTime as Date)
 
-// Calc
-var results = [Int]()
-var singlePos = [Int]()
-var fuelArray = [Int]()
-
-for crab in 0..<crabPosition.max()! {
-    singlePos = Array(repeating: crab, count: crabPosition.count)
-    fuelArray = zip(crabPosition, singlePos).map{abs($0 - $1)}
-    fuelArray = fuelArray.map{sumDict[$0]!}
-    results.append(fuelArray.reduce(0, +))
-    print(crab, crabPosition.max()!)
-}
-
-print("Part 1 | 2: ", results.min()!)
+print(interval)
---
+++ 2021.playground/Pages/Day7.xcplaygroundpage/Sources/main.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public class PlayGround {
+    public class func run() {
+        // Import data
+        let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
+        let crabPosition = try! String(contentsOf: url).replacingOccurrences(of: "\n", with: "").split(separator: ",").compactMap{ Int($0) }
+
+        // Precalculate sums
+        var sumDict: [Int: Int] = [:]
+        var sum = 0
+
+        for i in 0...crabPosition.max()! {
+            sum += i
+            sumDict[i] = sum
+        }
+
+        // Calc
+        var results = [Int]()
+        var singlePos = [Int]()
+        var fuelArray = [Int]()
+
+        for crab in 0..<crabPosition.max()! {
+            singlePos = Array(repeating: crab, count: crabPosition.count)
+            fuelArray = zip(crabPosition, singlePos).map{abs($0 - $1)}
+            fuelArray = fuelArray.map{sumDict[$0]!}
+            results.append(fuelArray.reduce(0, +))
+            print(crab, crabPosition.max()!)
+        }
+
+        // Print results
+        print("Part 1 | 2: ", results.min()!)
+    }
+}
