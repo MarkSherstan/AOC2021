45d4d89fdfca203df3fdd4c4d13bc6640dd035bf
Mark Sherstan <mshersta@ualberta.ca>
2021-12-21 21:33:54 -0700

    Day 7 Part 2

--- 2021.playground/Pages/Day7.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day7.xcplaygroundpage/Contents.swift
@@ -4,14 +4,26 @@ import Foundation
 let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
 let crabPosition = try String(contentsOf: url).replacingOccurrences(of: "\n", with: "").split(separator: ",").compactMap{ Int($0) }
 
-// Calc
-var results = [Int]()
+// Precalculate sums
+var sumDict: [Int: Int] = [:]
+var sum = 0
 
-for crab in crabPosition {
-    let singlePos = (Array(repeating: crab, count: crabPosition.count))
-    let fuelArray = (zip(crabPosition, singlePos).map{abs($0 - $1)})
-    let fuel = (fuelArray.reduce(0, +))
-    results.append(fuel)
+for i in 0...crabPosition.max()! {
+    sum += i
+    sumDict[i] = sum
 }
 
-print("Part 1: ", results.min()!)
+// Calc
+var results = [Int]()
+var singlePos = [Int]()
+var fuelArray = [Int]()
+
+for crab in 0..<crabPosition.max()! {
+    singlePos = Array(repeating: crab, count: crabPosition.count)
+    fuelArray = zip(crabPosition, singlePos).map{abs($0 - $1)}
+    fuelArray = fuelArray.map{sumDict[$0]!}
+    results.append(fuelArray.reduce(0, +))
+    print(crab, crabPosition.max()!)
+}
+
+print("Part 1 | 2: ", results.min()!)
