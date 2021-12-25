4cbb6f4d288a77ac2d09f048f9c61c7ae94d457e
Mark Sherstan <mshersta@ualberta.ca>
2021-12-25 12:22:23 -0700

    Day 8 Part 2

--- 2021.playground/Pages/Day8.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day8.xcplaygroundpage/Contents.swift
@@ -16,7 +16,7 @@ for dat in dataSplit {
     output.append(String(temp[1]))
 }
 
-// Process
+// Part 1
 var total = 0
 
 for out in output {
@@ -31,3 +31,75 @@ for out in output {
 
 // Print results
 print("Part 1:", total)
+
+// Part 2
+var values = [Int]()
+
+for (pat, out) in zip(pattern, output) {
+    // Split the data again and initialize the mapping
+    var temp = pat.split(separator: " ")
+    var mapping: [Int: Substring] = [:]
+    
+    // Get the easy values
+    for digit in temp {
+        switch digit.count {
+        case 2:
+            mapping[1] = digit
+        case 3:
+            mapping[7] = digit
+        case 4:
+            mapping[4] = digit
+        case 7:
+            mapping[8] = digit
+        default:
+            ()
+        }
+    }
+    
+    // Do the more difficult values
+    for digit in temp {
+        if digit.count == 5 {
+            // Create new comparison
+            var eight = Set(mapping[8]!)
+            let four = Array(mapping[4]!)
+            eight.subtract(four)
+            let eightMinusFour = String(Array(eight))
+ 
+            if mapping[1]!.allSatisfy(Array(digit).contains) {
+                mapping[3] = digit
+            } else if eightMinusFour.allSatisfy(Array(digit).contains) {
+                mapping[2] = digit
+            } else {
+                mapping[5] = digit
+            }
+        } else if digit.count == 6 {
+            if !mapping[1]!.allSatisfy(Array(digit).contains) {
+                mapping[6] = digit
+            } else if mapping[4]!.allSatisfy(Array(digit).contains) {
+                mapping[9] = digit
+            } else {
+                mapping[0] = digit
+            }
+        }
+    }
+    
+    // Split the output data and initialize a few more variables
+    var numBuilder = [String]()
+    temp = out.split(separator: " ")
+    numBuilder = [String]()
+    
+    // Loop and store the decoded values
+    for digit in temp {
+        for i in 0...9 {
+            if Array(mapping[i]!).sorted() == Array(digit).sorted() {
+                numBuilder.append(String(i))
+            }
+        }
+    }
+    
+    // Save the string to actual int
+    values.append(Int(numBuilder.joined(separator: ""))!)
+}
+
+// Results
+print("Part 2: ", values.reduce(0, +))
