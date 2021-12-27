01ab8717558795070ceee6cd39d2aaed9e651194
Mark Sherstan <mshersta@ualberta.ca>
2021-12-27 13:28:03 -0700

    Day 6 cleanup

--- 2021.playground/Pages/Day6.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day6.xcplaygroundpage/Contents.swift
@@ -2,9 +2,11 @@ import Foundation
 
 let startTime = NSDate()
 
-PlayGround.run()
+let day6 = Day6()
+day6.part1()
+day6.part2()
 
 let endTime = NSDate()
 let interval = endTime.timeIntervalSince(startTime as Date)
 
-print(interval)
+print("Time (seconds): ", String(format:"%.3f", interval))
--- 2021.playground/Pages/Day6.xcplaygroundpage/Sources/main.swift
+++ 2021.playground/Pages/Day6.xcplaygroundpage/Sources/main.swift
@@ -1,85 +1,100 @@
 import Foundation
 
-public class PlayGround {
-    public class func run() {
-        // Import data
+// Fish class
+class LanternFish {
+    var timer: Int
+    
+    init(timer: Int) {
+        self.timer = timer
+    }
+        
+    func age() -> Bool {
+        if self.timer == 0 {
+            self.timer = 6
+            return true
+        } else {
+            self.timer -= 1
+            return false
+        }
+    }
+}
+
+public class Day6 {
+    var timers: [Int]
+    
+    // Read in data
+    public init() {
         let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
-        let timers = try! String(contentsOf: url).replacingOccurrences(of: "\n", with: "").split(separator: ",").compactMap{ Int($0) }
+        self.timers = try! String(contentsOf: url).replacingOccurrences(of: "\n", with: "").split(separator: ",").compactMap{ Int($0) }
+    }
+    
+    // Time array builder
+    func timerBuilder(timer: Int, days: Int) -> [Int] {
+        var lanternFishArray = [LanternFish(timer: timer)]
 
-        // Fish class
-        class LanternFish {
-            var timer: Int
-            
-            init(timer: Int) {
-                self.timer = timer
-            }
-                
-            func age() -> Bool {
-                if self.timer == 0 {
-                    self.timer = 6
-                    return true
-                } else {
-                    self.timer -= 1
-                    return false
-                }
-            }
-        }
-
-        // Time array builder
-        func timerBuilder(timer: Int, days: Int) -> [Int] {
-            var lanternFishArray = [LanternFish(timer: timer)]
-
-            for _ in 0..<days {
-                for fish in lanternFishArray {
-                    if fish.age() {
-                        (lanternFishArray.append(LanternFish(timer: 8)))
-                    }
-                }
-            }
-            
-            var timerArray = [Int]()
-            
+        for _ in 0..<days {
             for fish in lanternFishArray {
-                timerArray.append(fish.timer)
+                if fish.age() {
+                    (lanternFishArray.append(LanternFish(timer: 8)))
+                }
             }
-            
-            return timerArray
         }
+        
+        var timerArray = [Int]()
+        
+        for fish in lanternFishArray {
+            timerArray.append(fish.timer)
+        }
+        
+        return timerArray
+    }
 
-        // Split progress into two sectors
-        var QQ: [Int: Int] = [:]
-        var QQQ: [Int: Int] = [:]
+    // Simulate the expotential growth
+    func simulate(numDays: Int) -> Int {
+        // Dictionary init
+        var growth50: [Int: Int] = [:]
+        var growth100: [Int: Int] = [:]
 
-        // Build array
+        // Calculate the first half of simulation for every age of fish and save to dictionary
         var master = [[Int]]()
 
         for i in 0...8 {
             master.append([Int]())
-            master[i].append(contentsOf: timerBuilder(timer: i, days: 128)) // Change to 80 for part 1
+            master[i].append(contentsOf: timerBuilder(timer: i, days: numDays/2))
         }
 
         for i in 0...8 {
-            QQ[i] = master[i].count
+            growth50[i] = master[i].count
         }
 
-        // Full progress
+        // Use the results of the first half to calculate the second half of simulation
         for i in 0...8 {
             let temp = master[i]
             
             var total = 0
-            for A in temp {
-                total += QQ[A]!
+            for entry in temp {
+                total += growth50[entry]!
             }
             
-            QQQ[i] = total
+            growth100[i] = total
         }
 
-        // Result
+        // Use pre calculated numbers on actual data set
         var total = 0
         for timer in timers {
-            total += QQQ[timer]!
+            total += growth100[timer]!
         }
 
-        print(total)
+        return total
+    }
+    
+    // Part 1
+    public func part1() {
+        print("Part 1: ", simulate(numDays: 80))
+    }
+    
+    // Part 2
+    public func part2() {
+        print("Part 2: ", simulate(numDays: 256))
     }
 }
