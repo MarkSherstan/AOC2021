c680d1223c0a9de962c6296d13295dc0f70ecbb4
Mark Sherstan <mshersta@ualberta.ca>
2021-12-27 00:57:49 -0700

    Day 1 cleanup

--- 2021.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -1,30 +1,12 @@
-import Cocoa
+import Foundation
 
-// Import depths
-let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
-let text = try String(contentsOf: url).split(separator: "\n")
-let sonarDepths = text.compactMap{ Int($0) }
+let startTime = NSDate()
 
-// Window the depths
-var sonarDepthsWindow = [Int]()
+let day1 = Day1()
+day1.part1()
+day1.part2()
 
-for i in 0..<(sonarDepths.count-2) {
-    sonarDepthsWindow.append(sonarDepths[i] + sonarDepths[i+1] + sonarDepths[i+2])
-}
+let endTime = NSDate()
+let interval = endTime.timeIntervalSince(startTime as Date)
 
-// Depth counter function
-func depthCounter(sonarDepthArray: Array<Int>) -> Int {
-    var depthCount = 0
-    
-    for i in 0..<(sonarDepthArray.count-1) {
-        if sonarDepthArray[i+1] - sonarDepthArray[i] > 0 {
-            depthCount += 1
-        }
-    }
-    
-    return depthCount
-}
-
-// Print result
-print("Part 1: ", depthCounter(sonarDepthArray: sonarDepths))
-print("Part 2: ", depthCounter(sonarDepthArray: sonarDepthsWindow))
+print("Time (seconds): ", String(format:"%.3f", interval))
