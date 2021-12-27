d41252fe8d2db82bb93b3a51640e14675abc1dac
Mark Sherstan <mshersta@ualberta.ca>
2021-12-27 01:18:22 -0700

    Day 2 cleanup

--- 2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -1,45 +1,12 @@
 import Foundation
 
-// Import directions
-let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
-let directions = try String(contentsOf: url).split(separator: "\n")
+let startTime = NSDate()
 
-// Initialize counter
-struct Position {
-    var horizontal = 0;
-    var depth = 0;
-    var aim = 0;
-}
+let day2 = Day2()
+day2.part1()
+day2.part2()
 
-var position = Position()
+let endTime = NSDate()
+let interval = endTime.timeIntervalSince(startTime as Date)
 
-// Loop through directions
-for direction in directions {
-    switch direction {
-        case _ where direction.contains("forward"):
-            let separated = direction.split(separator: " ")
-            position.horizontal += Int(separated[1])!
-        
-            // Part 2
-            position.depth += position.aim * Int(separated[1])!
-        case _ where direction.contains("up"):
-            let separated = direction.split(separator: " ")
-//            Part 1
-//            position.depth -= Int(separated[1])!
-        
-            // Part 2
-            position.aim -= Int(separated[1])!
-        case _ where direction.contains("down"):
-            let separated = direction.split(separator: " ")
-//            Part 1
-//            position.depth += Int(separated[1])!
-        
-            // Part 2
-            position.aim += Int(separated[1])!
-        default:
-            print("Error")
-        }
-}
-
-// Print result
-print("Part 1 and 2: ", position.horizontal * position.depth)
+print("Time (seconds): ", String(format:"%.3f", interval))
