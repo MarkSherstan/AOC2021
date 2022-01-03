6921dc5e1e30acd043678a478aeb2f2bac957dc0
Mark Sherstan <mshersta@ualberta.ca>
2022-01-02 22:57:39 -0700

    Day 15 Part 1

--- 2021.playground/Pages/Day15.xcplaygroundpage/Sources/main.swift
+++ 2021.playground/Pages/Day15.xcplaygroundpage/Sources/main.swift
@@ -3,59 +3,99 @@ import Foundation
 // Matrix structure with reference data
 struct Matrix {
     var matrix: [[Int]]
-    var rows: Int?
-    var cols: Int?
+    var rows: Int!
+    var cols: Int!
+    var dis: [[Int]]!
     
     init(matrix: [[Int]]) {
         self.matrix = matrix
         self.rows = matrix.count
         self.cols = matrix[0].endIndex
+        self.dis = Array(repeating: Array(repeating: Int.max, count: self.cols!), count: self.rows!)
     }
 }
 
+// Hold cell data -> posible protocols: Hashable, Equatable
+struct Cell {
+    var x: Int
+    var y: Int
+    var distance: Int
+}
+
+// Main logic
 public class Day15 {
     var matrix: Matrix
-
+    
     // Read in data
     public init() {
         let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
         let input = try! String(contentsOf: url).split(separator: "\n")
-
+        
         var mat = [[Int]]()
         for row in input {
             mat.append(row.compactMap{$0.wholeNumberValue})
         }
-
+        
         self.matrix = Matrix(matrix: mat)
     }
     
-    func minCost(matrix: Matrix, row: Int, col: Int) -> Int {
-        var dp = Array(repeating: Array(repeating: 0, count: matrix.cols!), count: matrix.rows!)
+    // Dijkstra algorithm
+    func djk(matrix: inout Matrix) -> Int {
+        // Directions to check
+        let dx = [1, 0, -1, 0]
+        let dy = [0, 1, 0, -1]
         
-        for i in 1...row {
-            dp[i][0] = dp[i-1][0] + matrix.matrix[i][0]
-        }
+        // Cell array structure
+        var st = [Cell]()
+        st.append(Cell(x: 0, y: 0, distance: 0))
+        
+        // First cell is not entered, set to zero
+        matrix.dis![0][0] = 0
+        
+        while !st.isEmpty {
+            // Get cell with min distance and then delete it from array (always first entry)
+            let k = st.removeFirst()
+            
+            // Loop through neighbours
+            for (xx, yy) in zip(dx, dy) {
+                let x = k.x + xx;
+                let y = k.y + yy;
                 
-        for j in 1...col {
-            dp[0][j] = dp[0][j-1] + matrix.matrix[0][j]
-        }
+                // Check boundaries
+                if (x < 0 || x >= matrix.rows!) {continue}
+                if (y < 0 || y >= matrix.cols!) {continue}
                 
-        for i in 1...row {
-            for j in 1...col {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + matrix.matrix[i][j]
+                // If distance from current cell is smaller, then update distance of neighbour cell
+                if (matrix.dis[x][y] > matrix.dis[k.x][k.y] + matrix.matrix[x][y]) {
+                    
+                    // Update distance and insert updated cell in array
+                    matrix.dis[x][y] = matrix.dis[k.x][k.y] + matrix.matrix[x][y];
+                    st.append(Cell(x: x, y: y, distance: matrix.dis[x][y]));
+                }
+            }
+            
+            // Sort the array of structs
+            st.sort { (a: Cell, b: Cell) -> Bool in
+                if a.distance == b.distance {
+                    if a.x != b.x {
+                        return a.x < b.x
+                    } else {
+                        return a.y < b.y
+                    }
+                }
+                return a.distance < b.distance
             }
         }
-                
-        return dp[row][col]
+        
+        // Solution
+        return matrix.dis[matrix.rows - 1][matrix.cols - 1]
     }
-
-    // That's not the right answer; your answer is too high. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 402.) [Return to Day 15]
-                    
+        
     // Part 1
     public func part1() {
-        print("Part 1: ", minCost(matrix: matrix, row: matrix.rows!-1, col: matrix.cols!-1))
+        print("Part 1: ", djk(matrix: &matrix))
     }
-
+    
     // Part 2
     public func part2() {
         print("Part 2: ", 000)
