86aeda71370307c32f626f68d324a857e6b779d6
Mark Sherstan <mshersta@ualberta.ca>
2022-01-12 20:52:49 -0700

    Literal portion with example data

--- 2021.playground/Pages/Day16.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day16.xcplaygroundpage/Contents.swift
@@ -1,7 +1,42 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// Split up array into chunks
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
 
-//: [Next](@next)
+// Import data
+let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
+let msgHex = try String(contentsOf: url).split(separator: "\n")
+var msgBin = Array(String(Int(msgHex[0], radix: 16)!, radix: 2)) // Make sure this doesent overflow later
+
+// Header Data
+let packetVerion = Int(String(msgBin[0..<3]), radix: 2)!
+let packetType = Int(String(msgBin[3..<6]), radix: 2)!
+
+// Get literal value
+func literal(msg: [Character]) -> Int {
+    let msgLocal = Array(msg.dropFirst(6))
+    let msgChunk = msgLocal.chunked(into: 5)
+    var bits = [Character]()
+    
+    for chunk in msgChunk {
+        if chunk[0] == "1" {
+            bits.append(contentsOf: chunk[1..<5])
+        } else {
+            bits.append(contentsOf: chunk[1..<5])
+            break
+        }
+    }
+    
+    return Int(String(bits), radix: 2)!
+}
+
+// Literal function
+if packetType == 4 {
+    print(literal(msg: msgBin))
+}
