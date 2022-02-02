2fc3eb9c09d23841084cdc6cb2e3eb5cedf5ac8b
Mark Sherstan <mshersta@ualberta.ca>
2022-02-01 19:29:12 -0700

    Initial logic for switching directions complete.

--- 2021.playground/Pages/Day19.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day19.xcplaygroundpage/Contents.swift
@@ -35,5 +35,83 @@ for Entry in raw {
     scannerArray.append(Scanner(ID: ID, A: A, B: B, C: C))
 }
 
-// Print
-print(scannerArray.count, scannerArray[0].ID, scannerArray[0].A)
+// Find mode of array
+func mode(numbers: [Int]) -> Int {
+    var occurrences: [Int: Int] = [:]
+    for number in numbers {
+        if occurrences[number] == nil {
+            occurrences[number] = 1
+        } else {
+            occurrences[number]! += 1
+        }
+    }
+
+    return occurrences.values.max()!
+}
+
+// Function for checking directions
+func directionMachine(littleScanner: [Int], masterScanner: [Int]) -> Int {
+    // Variable declaration
+    var tempArray = [Int]()
+    
+    // Check positive case
+    for little in littleScanner {
+        tempArray.append(contentsOf: masterScanner.map{$0 + little})
+    }
+    
+    // Results and prep for next loop
+    if mode(numbers: tempArray) >= 12 {
+        return 1
+    } else {
+        tempArray.removeAll()
+    }
+    
+    // Check negative case
+    for little in littleScanner {
+        tempArray.append(contentsOf: masterScanner.map{$0 - little})
+    }
+    
+    // Results
+    if mode(numbers: tempArray) >= 12 {
+        return -1
+    } else {
+        return 0
+    }
+}
+
+let masterA = scannerArray[0].A
+let masterB = scannerArray[0].B
+let masterC = scannerArray[0].C
+
+let A = scannerArray[1].A
+let B = scannerArray[1].B
+let C = scannerArray[1].C
+
+var arrayA = [Int]()
+var arrayB = [Int]()
+var arrayC = [Int]()
+
+// Still need to switch columns
+for a in A {
+    arrayA.append(contentsOf: masterA.map{$0 + a})
+}
+
+for b in B {
+    arrayB.append(contentsOf: masterB.map{$0 - b})
+}
+
+for c in C {
+    arrayC.append(contentsOf: masterC.map{$0 + c})
+}
+
+print(mode(numbers: arrayA), mode(numbers: arrayB), mode(numbers: arrayC))
+print(directionMachine(littleScanner: A, masterScanner: masterA))
+print(directionMachine(littleScanner: B, masterScanner: masterB))
+print(directionMachine(littleScanner: C, masterScanner: masterC))
+
+// Once we find a match... Need to make a master array for comparison with everything orientated properly
+// First grouping is just +/-
+// Still need to account for swapping
+// Make a function....
+
+//print(scannerArray.count, scannerArray[0].ID, scannerArray[0].A)
