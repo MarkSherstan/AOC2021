097a8ff3acc8cfcf8267c6d2f8865db810d5aec0
Mark Sherstan <mshersta@ualberta.ca>
2022-03-20 19:34:15 -0600

    Day 11 cleanup

--- 2021.playground/Pages/Day11.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day11.xcplaygroundpage/Contents.swift
@@ -1,94 +1,12 @@
 import Foundation
 
-// Matrix structure with quick functions and reference data
-struct Matrix {
-    var matrix: [[Int]]
-    var rows: Int?
-    var cols: Int?
-    var boolMatrix: [[Int]]?
-    
-    init(matrix: [[Int]]) {
-        self.matrix = matrix
-        self.rows = matrix.count
-        self.cols = matrix[0].endIndex
-        self.boolMatrix = Array(repeating: Array(repeating: 0, count: self.cols!), count: self.rows!)
-    }
-        
-    mutating func resetBoolMatrix() {
-        self.boolMatrix = Array(repeating: Array(repeating: 0, count: self.cols!), count: self.rows!)
-    }
-}
+let startTime = NSDate()
 
-// Import data
-let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
-let input = try String(contentsOf: url).split(separator: "\n")
+let day11 = Day11()
+day11.part1()
+day11.part2()
 
-var mat = [[Int]]()
-for row in input {
-    mat.append(row.compactMap{$0.wholeNumberValue})
-}
+let endTime = NSDate()
+let interval = endTime.timeIntervalSince(startTime as Date)
 
-// Global variables
-var matrix = Matrix(matrix: mat)
-var counter = 0
-
-// Flash function
-func flash(x: Int, y: Int) {
-    // Out of bounds
-    if (x < 0 || x == matrix.rows!) {return}
-    if (y < 0 || y == matrix.cols!) {return}
-  
-    // Increment
-    matrix.matrix[x][y] += 1
-    
-    // Can flash and already flashed checks
-    if (matrix.boolMatrix![x][y] == 1) {return}
-    if (matrix.matrix[x][y] < 10) {return}
-    
-    // Mark the current cell as flashed and increment counter
-    matrix.boolMatrix![x][y] = 1;
-    counter += 1
-
-    // Directions
-    let dx = [1, 0, -1, 0, 1, -1, 1, -1]
-    let dy = [0, 1, 0, -1, 1, -1, -1, 1]
-    
-    // Recursively increment and check the neighbors
-    for (dirX, dirY) in zip(dx, dy) {
-        flash(x: x+dirX, y: y+dirY)
-    }
-}
-
-// Main simulation
-var step = 1
-while(true) {
-    // Perform the flashes
-    for row in 0..<matrix.rows! {
-        for col in 0..<matrix.cols! {
-           flash(x: row, y: col)
-        }
-    }
-    
-    // All flashed octopus go to zero
-    for row in 0..<matrix.rows! {
-        for col in 0..<matrix.cols! {
-            if matrix.boolMatrix![row][col] == 1 {
-                matrix.matrix[row][col] = 0
-            }
-        }
-    }
-    matrix.resetBoolMatrix()
-    
-    // Exit conditions
-    if step == 100 {
-        print("Part 1: ", counter)
-    }
-    
-    if Set(matrix.matrix.reduce([], +)).count == 1 {
-        print("Part 2: ", step)
-        break
-    }
-    
-    // Increment step count
-    step += 1
-}
+print("Time (seconds): ", String(format:"%.3f", interval))
