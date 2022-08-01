c1fcd830bb91a9737403dce245bab2b6f1fe7c1c
Mark Sherstan <mshersta@ualberta.ca>
2022-08-01 12:42:12 -0600

    Initial commit

---
+++ Day22/Day22/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day22
+//
+//  Created by Mark Sherstan on 2022-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day22/Day22/Day22App.swift
@@ -0,0 +1,17 @@
+//
+//  Day22App.swift
+//  Day22
+//
+//  Created by Mark Sherstan on 2022-08-01.
+//
+
+import SwiftUI
+
+@main
+struct Day22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
