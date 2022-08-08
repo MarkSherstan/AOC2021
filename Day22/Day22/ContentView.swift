ba840d2a8d11363b38ea9941b9acb4e9ba57eea0
Mark Sherstan <mshersta@ualberta.ca>
2022-08-07 22:27:49 -0600

    Remove test options

--- Day22/Day22/ContentView.swift
+++ Day22/Day22/ContentView.swift
@@ -9,28 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var d22 = Day22()
-    @State var test1Result: String = ""
-    @State var test2Result: String = ""
     @State var part1Result: String = ""
     @State var part2Result: String = ""
     @State var timeResult: String = "Time Elapsed [s]: "
     
     var body: some View {
         VStack(spacing: 40) {
-            HStack(spacing: 50) {
-                VStack(spacing: 20) {
-                    VStack(spacing: 20) {
-                        Button(action: {
-                            let result = self.d22.updateTest1()
-                            test1Result = String(result.sol)
-                            timeResult = "Time Elapsed [s]: " + String(format: "%.3f", result.time)
-                        }) {
-                            Text("Test 1")
-                        }
-                        
-                        Text(test1Result)
-                    }
-                    
+            VStack(spacing: 20) {
+                HStack(spacing: 50) {
                     VStack(spacing: 20) {
                         Button(action: {
                             let result = self.d22.updatePart1()
@@ -39,22 +25,7 @@ struct ContentView: View {
                         }) {
                             Text("Part 1")
                         }
-                        
                         Text(part1Result)
-                    }
-                }
-                
-                VStack(spacing: 20) {
-                    VStack(spacing: 20) {
-                        Button(action: {
-                            let result = self.d22.updateTest2()
-                            test2Result = String(result.sol)
-                            timeResult = "Time Elapsed [s]: " + String(format: "%.3f", result.time)
-                        }) {
-                            Text("Test 2")
-                        }
-                        
-                        Text(test2Result)
                     }
                     
                     VStack(spacing: 20) {
@@ -65,12 +36,11 @@ struct ContentView: View {
                         }) {
                             Text("Part 2")
                         }
-                        
                         Text(part2Result)
                     }
                 }
+                Text(timeResult)
             }
-            Text(timeResult)
         }
     }
 }
