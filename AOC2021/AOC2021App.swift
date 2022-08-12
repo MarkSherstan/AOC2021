cf17b199b76a374f6d6db654b143246a1d837081
Mark Sherstan <mshersta@ualberta.ca>
2022-08-11 21:36:06 -0600

    Initial commit

---
+++ AOC2021/AOC2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2021App.swift
+//  AOC2021
+//
+//  Created by Mark Sherstan on 2022-08-11.
+//
+
+import SwiftUI
+
+@main
+struct AOC2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AOC2021
+//
+//  Created by Mark Sherstan on 2022-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
