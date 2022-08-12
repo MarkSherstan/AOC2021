f889c0091e07a96f833dfacc89839b1b4304fe61
Mark Sherstan <mshersta@ualberta.ca>
2022-08-11 22:13:41 -0600

    Rough picker example

--- AOC2021/ContentView.swift
+++ AOC2021/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = "R"
+    let colors: [String: String] = ["R": "Red", "G": "Green"]
+    let colorKeyArray: [String]
+    
+    init() {
+        self.colorKeyArray = Array(self.colors.keys)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker("Select a paint color", selection: $selection) {
+                ForEach(colorKeyArray, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.menu)
+
+            Text("Selected color: \(colors[selection]!)")
+        }
     }
 }
 
