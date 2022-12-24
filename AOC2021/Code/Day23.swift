f81a6e81d6cb6be5827b01f8c20f70321fbae031
Mark Sherstan <mshersta@ualberta.ca>
2022-12-23 19:19:52 -0700

    Getting initial thoughts down.

--- AOC2021/Code/Day23.swift
+++ AOC2021/Code/Day23.swift
@@ -8,16 +8,35 @@
 import Foundation
 
 class Day23 {
-    init() {
-        // Load in data
-        let url = Bundle.main.url(forResource: "Day23", withExtension: "txt")!
-        let input = try! String(contentsOf: url).split(separator: "\n")
+    let roomLocation: [String: Int] = ["A": 2, "B": 4, "C": 6, "D": 8]
+    let moveEnergy: [String: Int] = ["A": 1, "B": 10, "C": 100, "D": 1000]
+    
+//    init() {
+//        // Load in data (do this parsing later)
+//        let url = Bundle.main.url(forResource: "Day23", withExtension: "txt")!
+//        let input = try! String(contentsOf: url).split(separator: "\n")
+//    }
+    
+    func solve(_ board: [String]) -> Int {
+        var states: [[String]: Int] = [board: 0]
         
-        print(input)
+        var queue = [board]
+        while !queue.isEmpty {
+            let board = queue.removeLast()
+
+                    
+            print(board)
+        }
+        
+        return 1
     }
     
     func part1() -> String {
-        return "A"
+        let board = [".", ".", "BA", ".", "CD", ".", "BC", ".", "DA", ".", "."]
+        
+        let sol = solve(board)
+        print(sol)
+        return String(sol)
     }
     
     func part2() -> String {
