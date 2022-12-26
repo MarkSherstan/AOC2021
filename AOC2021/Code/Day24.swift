5221f48ce4432962dfb2258ea98825b37db12ae6
Mark Sherstan <mshersta@ualberta.ca>
2022-12-26 15:01:48 -0700

    Add skeleton for Day24

---
+++ AOC2021/Code/Day24.swift
@@ -0,0 +1,27 @@
+//
+//  Day24.swift
+//  AOC2021
+//
+//  Created by Mark Sherstan on 2022-12-26.
+//
+
+import Foundation
+
+class Day24 {
+    init() {
+        // Load in data
+        let url = Bundle.main.url(forResource: "Day24", withExtension: "txt")!
+        let input = try! String(contentsOf: url).split(separator: "\n")
+        print(input)
+    }
+    
+    /// Part 1
+    func part1() -> String {
+        return "A"
+    }
+    
+    /// Part 2
+    func part2() -> String {
+        return "B"
+    }
+}
